0af754c8945a8193dd60654b8a1e2d5b7e234948
Artasov <ivanhvalevskey@gmail.com>
2023-12-01 04:39:56 +0300

    fix

--- Core/urls.py
+++ Core/urls.py
@@ -3,13 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
-from django.views.generic import TemplateView
+from Core.views.common_views import example
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('terms_and_conditions/', TemplateView.as_view(template_name='Core/terms_and_conditions.html'),
-         name='terms_and_conditions'),
-    path('privacy_policy/', TemplateView.as_view(template_name='Core/privacy_policy.html'), name='privacy_policy'),
+    path('', example),
 
 ]
 if settings.DEBUG:
