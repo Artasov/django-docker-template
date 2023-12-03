a923763ae94752b2e89af734a948609795dabefd
Artasov <ivanhvalevskey@gmail.com>
2023-12-04 02:35:34 +0300

    fix

--- Core/views/common_views.py
+++ Core/views/common_views.py
@@ -1,12 +1,7 @@
 import logging
 from random import randint
-
-from django.core.cache import cache
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render
 from django_redis import get_redis_connection
-
-from Core.models import User
-
 from Core.services.services import base_view
 from Core.tasks import test_task
 
