6a873fefbd9fb327612e29aabd7e6e34d02918d1
Artasov <ivanhvalevskey@gmail.com>
2023-12-05 08:34:47 +0300

    fix

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -8,10 +8,12 @@ mkdir -p /srv/celerybeat-volume/
 touch /srv/celerybeat-volume/celerybeat-schedule && chmod 666 /srv/celerybeat-volume/celerybeat-schedule
 touch /srv/celerybeat-volume/celerybeat.pid  && chmod 666 /srv/celerybeat-volume/celerybeat-schedule
 
+#source /srv/venv/bin/activate
+
 # Collect static files into one folder without asking for confirmation
 python manage.py collectstatic --noinput &&
 # Apply migrations to the database
-python manage.py migrate &&
+python manage.py migrate
 # Bind gunicorn
-su -l base_user -c "gunicorn config.wsgi:application --workers 1 --bind 0.0.0.0:8000 --timeout 60 --max-requests 1000"
+gunicorn config.wsgi:application --workers 1 --bind 0.0.0.0:8000 --timeout 60 --max-requests 1000
 
